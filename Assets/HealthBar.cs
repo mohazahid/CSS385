24456da998dea7fb2cf1805fdb334faf5ef94731
mohazahid <95829594+mohazahid@users.noreply.github.com>
2022-10-02 19:26:14 -0700

    Add files via upload

---
+++ Assets/HealthBar.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider slider;
+    public Gradient gradient;
+    public Image fill;
+    [SerializeField]
+    private Text HealthValue;
+
+    public void SetMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+        HealthValue.text = health.ToString()+ "/100";
+        fill.color = gradient.Evaluate(1f);
+    }
+
+    public void SetHealth(int health)
+    {
+        if (health < 10) {
+            HealthValue.text = "0/100";
+        } else {
+            HealthValue.text = health.ToString()+ "/100";
+        }
+        slider.value = health;
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+}
